36c1c530f609e0384dfa58471d9e57d15843d732
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-08-12 17:35:51 -0500

    trying to fix static path

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + "public"))
 
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, './Develop/public/index.html'))
