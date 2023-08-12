4bc15cbd0ac368639c49e1c1c50405e6d1ed2b94
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-08-12 14:28:57 -0500

    added the code for getting and posting a note and then exported to index.js

---
+++ Develop/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const noteRouter = require('./notes')
+
+
+const app = express();
+
+app.use('/notes', noteRouter);
+
+
+module.exports = app;
---
+++ Develop/routes/notes.js
@@ -0,0 +1,26 @@
+const note = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+note.get('/notes', (req, res) => {
+    readFromFile('./Develop/db/notes.json').then((data) => res.json(JSON.parse(data)));
+})
+
+note.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            text_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './Develop/db/notes.json');
+        res.json(`New Note! New Goals!`);
+    } else {
+        res.errored('Too bad we could not make a note to fix this error :/')
+    }
+});
+
+module.exports = note;
